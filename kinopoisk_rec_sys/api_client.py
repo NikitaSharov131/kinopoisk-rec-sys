dff6d4f5b40685014ccae77d4249add29f8915e3
NikitaSharov <nikita.sharov@aviasales.ru>
2024-07-14 21:03:46 +0300

    feat(search): add recommended movie entity

--- kinopoisk_rec_sys/api_client.py
+++ kinopoisk_rec_sys/api_client.py
@@ -5,15 +5,15 @@ import requests
 from attrs import define, AttrsInstance, fields
 from datetime import datetime, timedelta
 from functools import cached_property
-from typing import List
+from typing import List, Optional
 
 
 @define(frozen=True)
 class Rating:
-    kp: int
-    imdb: int
-    filmCritics: int
-    russianFilmCritics: int
+    kp: float
+    imdb: Optional[float] = None
+    filmCritics: Optional[float] = None
+    russianFilmCritics: Optional[float] = None
 
 
 @define(frozen=True)
@@ -27,17 +27,22 @@ class Votes:
 @define(frozen=True)
 class Genre:
     name: str
-    slug: str
+    slug: str = None
 
 
-@define(frozen=True)
+@define(frozen=True, kw_only=True)
+class User:
+    id: str
+    preferred_genres: List[Genre.name]
+    min_movies_rating: Rating
+
+
+@define(frozen=True, kw_only=True)
 class Movie(AttrsInstance):
     id: int
     name: str
     alternativeName: str
     enName: str
-    type: str
-    typeNumber: int
     year: int
     description: str
     shortDescription: str
@@ -46,17 +51,22 @@ class Movie(AttrsInstance):
     votes: Votes
     movieLength: int
     ratingMpaa: str
-    ageRating: int
     genres: List[Genre] = None
 
 
-@define(frozen=True)
+@define(frozen=True, kw_only=True)
+class RecommendedMovie(Movie):
+    user_id: User.id
+    kp_url: str
+
+
+@define()
 class MovieQuery(AttrsInstance):
     type: str = "movie"
     page: int = 1
-    limit: int = 250
+    limit: int = 10
     isSeries: str = "false"
-    selectFields: List = [f.name for f in fields(Movie)]
+    selectFields: List = [f.name for f in fields(Movie) if f.name not in ("kp_url")]
 
 
 @define(frozen=True)
@@ -69,16 +79,22 @@ class NewMovieQuery(MovieQuery):
     updatedAt: str = (datetime.now() - timedelta(days=1)).strftime("%d.%m.%Y")
 
 
+def _assign_rating(rating: Rating):
+    return {rating_kind: f"{rating_value}-10" for rating_value, rating_kind in
+            zip([rating.kp, rating.imdb], ["rating.kp", "rating.imdb"]) if rating_value}
+
+
 class KinopoiskApiClient:
     movie_meta = []
 
     def __init__(self):
         self._api_key = os.environ.get('kp_key')
         self.base_url = 'https://api.kinopoisk.dev'
+        self.base_kp_url = 'https://www.kinopoisk.ru/film/'
 
     def get_possible_genres(self) -> List[Genre]:
         endpoint = 'movie/possible-values-by-field'
-        if genres := self._get(endpoint, PossibleValues(field='genres.name'), 'v1'):
+        if genres := self._get(endpoint, attrs.asdict(PossibleValues(field='genres.name')), 'v1'):
             return [Genre(**genre) for genre in genres]
 
     @cached_property
@@ -87,9 +103,9 @@ class KinopoiskApiClient:
         session.headers = {'X-API-KEY': self._api_key, "accept": "application/json"}
         return session
 
-    def _get(self, endpoint, query_params: attrs.Factory, api_version: str = 'v1.4'):
+    def _get(self, endpoint, query_params: dict, api_version: str = 'v1.4'):
         url = f"{self.base_url}/{api_version}/{endpoint}"
-        response = self._session.get(url, params=attrs.asdict(query_params))
+        response = self._session.get(url, params=query_params)
         try:
             response.raise_for_status()
         except requests.RequestException as exc:
@@ -101,12 +117,14 @@ class KinopoiskApiClient:
         endpoint = f"movie/{movie_id}"
         return self._get(endpoint)
 
-    def get_movie(self) -> List[Movie]:
+    def get_movie(self, user: User) -> List[RecommendedMovie]:
         endpoint = 'movie'
-        movies = []
-        if raw_movies := self._get(endpoint, MovieQuery()):
-            movies.extend([Movie(**movie) for movie in raw_movies.get("docs")])
-            return movies
+        query = attrs.asdict(MovieQuery())
+        query['genres.name'] = user.preferred_genres
+        query = {**query, **_assign_rating(user.min_movies_rating)}
+        if raw_movies := self._get(endpoint, query):
+            return [RecommendedMovie(**movie, **{"kp_url": f"{self.base_kp_url}/{movie.get('id')}", "user_id": user.id})
+                    for movie in raw_movies.get("docs")]
 
     def get_top_rated_movies(self, page=1):
         endpoint = 'movie/top_rated'
@@ -120,4 +138,6 @@ class KinopoiskApiClient:
 
 # Usage example
 if __name__ == "__main__":
-    ...
+    r = Rating(kp=10)
+    t = r.kp.__class__.__name__
+    print(t)
--- kinopoisk_rec_sys/search.py
+++ kinopoisk_rec_sys/search.py
@@ -1,17 +1,27 @@
-from kinopoisk_rec_sys.api_client import KinopoiskApiClient
-from kinopoisk_rec_sys.scylla import KinopoiskScyllaDB
+from kinopoisk_rec_sys.api_client import KinopoiskApiClient, Genre, User, Rating
+from kinopoisk_rec_sys.scylla import KinopoiskScyllaDB, Genre as ScyllaGenre, RecommendedMovie
+from typing import List
 
 
 class KinopoiskSearch:
+
     def __init__(self):
         self._api = KinopoiskApiClient()
         self._scylla = KinopoiskScyllaDB()
 
-    def collect_genres(self):
-        genres = self._api.get_possible_genres()
-        self._scylla.insert_collection(genres)
+    def collect_genres(self) -> List[Genre]:
+        genres = self._scylla.get_collection_elements(ScyllaGenre)
+        if not genres:
+            genres = self._api.get_possible_genres()
+            self._scylla.insert_collection(genres, ScyllaGenre)
+        return [Genre(**genre) for genre in genres]
+
+    def start_search(self):
+        user = User(id="12312333123123", preferred_genres=[Genre(name="драма").name], min_movies_rating=Rating(kp=8.1))
+        recommended_movies = self._api.get_movie(user)
+        self._scylla.insert_collection(recommended_movies, RecommendedMovie)
 
 
 if __name__ == '__main__':
     k = KinopoiskSearch()
-    k.collect_genres()
+    k.start_search()
